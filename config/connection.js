f3dc1b64b43944859f73d738922f7d8073d02fb6
Tien Nguyen <tien.ng1020@gmail.com>
2019-09-07 11:27:05 -0500

    connection

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,8 @@ var mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+} 
+else {
  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
