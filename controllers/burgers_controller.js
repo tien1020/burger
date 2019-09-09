a3c067da34926bbe47abfe9ef683982298249020
Tien Nguyen <tien.ng1020@gmail.com>
2019-09-08 22:08:53 -0500

    newburger

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,11 +8,11 @@ var burger = require("../models/burger.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
   burger.all(function(burgerData) {
-    // var hbsObject = {
-    //   burgers: data
-    // };
-    // console.log(hbsObject);
-    res.render("index", {burger_data: burgerData});
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
   });
 });
 
